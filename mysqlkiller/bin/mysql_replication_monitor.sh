ddc33dbef94f36c16c9bd3fc84820bfaaaaca8bc
waxmoth <waxmoth@qq.com>
2023-01-06 15:25:09 +0800

    DB replication monitor: Add Mail service to test the email

--- mysqlkiller/bin/mysql_replication_monitor.sh
+++ mysqlkiller/bin/mysql_replication_monitor.sh
@@ -6,6 +6,7 @@ MYSQL_HOST=${MYSQL_HOST:-'127.0.0.1'}
 MAX_REPLICATION_DELAY=${MAX_REPLICATION_DELAY:-300}
 EMAIL_TO=${EMAIL_TO:-''}
 EMAIL_FROM=${EMAIL_FROM:-'noreply@example.com'}
+SMTP_HOST=${SMTP_HOST:-'127.0.0.1'}
 
 export MYSQL_PWD=${MYSQL_PWD:-root}
 
@@ -27,7 +28,7 @@ secondsDelay=$(
 log "Checking the DB replication delay: ${secondsDelay}"
 
 if [[ ${secondsDelay} -gt ${MAX_REPLICATION_DELAY} ]]; then
-    sendmail -i -t "${EMAIL_TO}" -f "${EMAIL_FROM}" << EOL
+    sendmail -i -t "${EMAIL_TO}" -f "${EMAIL_FROM}" -S "${SMTP_HOST}" << EOL
 Subject: [ALERT] DB replication!
 
 MySQL replication delay, please check and fix it!
